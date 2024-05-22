7f2ea94564da80b85e360a7958e3f841b07aeaee
abhaysachan232 <abhaysachan232@gmail.com>
2024-05-22 18:50:50 +0530

    pilot update preload

--- src/PilotComp.tsx
+++ src/PilotComp.tsx
@@ -5,6 +5,8 @@ import { useEffect, useReducer, useState } from "react";
 import { GlobalContext, reducer } from "./Context";
 import Router from "./router/Router";
 import { BrowserRouter, HashRouter } from "react-router-dom";
+import { AsyncPreloader } from "async-preloader";
+
 
 function PilotComp() {
   /* eslint-disable react-hooks/exhaustive-deps */
@@ -39,16 +41,117 @@ function PilotComp() {
     const json = await response.json();
     const data = await json;
     setData(data);
-    // console.log(data);
-    
+    //  console.log(data);    
     setLowerCaseLetters(Object.keys(data["lowercase"]));
     setUpperCaseLetters(Object.keys(data["uppercase"]));
     setNumbers(Object.keys(data["num"]));
+    preload();
+
   };
+const items = [
+  {src:"/media/sprite/lowercase/a.png"},
+  {src:"/media/sprite/lowercase/b.png"},
+  {src:"/media/sprite/lowercase/c.png"},
+  {src:"/media/sprite/lowercase/d.png"},
+  {src:"/media/sprite/lowercase/e.png"},
+  {src:"/media/sprite/lowercase/f.png"},
+  {src:"/media/sprite/lowercase/g.png"},
+  {src:"/media/sprite/lowercase/h.png"},
+  {src:"/media/sprite/lowercase/i.png"},
+  {src:"/media/sprite/lowercase/j.png"},
+  {src:"/media/sprite/lowercase/k.png"},
+  {src:"/media/sprite/lowercase/l.png"},
+  {src:"/media/sprite/lowercase/m.png"},
+  {src:"/media/sprite/lowercase/n.png"},
+  {src:"/media/sprite/lowercase/o.png"},
+  {src:"/media/sprite/lowercase/p.png"},
+  {src:"/media/sprite/lowercase/q.png"},
+  {src:"/media/sprite/lowercase/r.png"},
+  {src:"/media/sprite/lowercase/s.png"},
+  {src:"/media/sprite/lowercase/t.png"},
+  {src:"/media/sprite/lowercase/u.png"},
+  {src:"/media/sprite/lowercase/v.png"},
+  {src:"/media/sprite/lowercase/w.png"},
+  {src:"/media/sprite/lowercase/x.png"},
+  {src:"/media/sprite/lowercase/y.png"},
+  {src:"/media/sprite/lowercase/z.png"},
+
+
+  {src:"/media/sprite/uppercase/A.png"},
+  {src:"/media/sprite/uppercase/B.png"},
+  {src:"/media/sprite/uppercase/C.png"},
+  {src:"/media/sprite/uppercase/D.png"},
+  {src:"/media/sprite/uppercase/E.png"},
+  {src:"/media/sprite/uppercase/F.png"},
+  {src:"/media/sprite/uppercase/G.png"},
+  {src:"/media/sprite/uppercase/H.png"},
+  {src:"/media/sprite/uppercase/I.png"},
+  {src:"/media/sprite/uppercase/J.png"},
+  {src:"/media/sprite/uppercase/K.png"},
+  {src:"/media/sprite/uppercase/L.png"},
+  {src:"/media/sprite/uppercase/M.png"},
+  {src:"/media/sprite/uppercase/N.png"},
+  {src:"/media/sprite/uppercase/O.png"},
+  {src:"/media/sprite/uppercase/P.png"},
+  {src:"/media/sprite/uppercase/Q.png"},
+  {src:"/media/sprite/uppercase/R.png"},
+  {src:"/media/sprite/uppercase/S.png"},
+  {src:"/media/sprite/uppercase/T.png"},
+  {src:"/media/sprite/uppercase/U.png"},
+  {src:"/media/sprite/uppercase/V.png"},
+  {src:"/media/sprite/uppercase/W.png"},
+  {src:"/media/sprite/uppercase/X.png"},
+  {src:"/media/sprite/uppercase/Y.png"},
+  {src:"/media/sprite/uppercase/Z.png"},
+
+  {src:"/media/sprite/num/1.png"},
+  {src:"/media/sprite/num/2.png"},
+  {src:"/media/sprite/num/3.png"},
+  {src:"/media/sprite/num/4.png"},
+  {src:"/media/sprite/num/5.png"},
+  {src:"/media/sprite/num/6.png"},
+  {src:"/media/sprite/num/7.png"},
+  {src:"/media/sprite/num/8.png"},
+  {src:"/media/sprite/num/9.png"},
+  {src:"/media/sprite/num/10.png"},
+
+]
+async function preload() {
+  await Promise.all(
+    items.map(async (item) => {
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const data = await AsyncPreloader.loadItems(process.env.PUBLIC_URL + item["src"]);
+      console.log(data);
+
+      // loadedCount++;
+      // console.log(`Progress: ${(100 * loadedCount) / preloadList.length}%`);
+
+      // let prcntg = (100 * loadedCount) / preloadList.length;
+      // setProgress(Math.floor(prcntg));
+
+      // if (prcntg === 100) {
+      //   setProgress(100);
+
+      //   setTimeout(() => {
+      //     setBounceOutDown(false);
+      //     // setShowLoading(false);
+      //     // setFade(true);
+      //   }, 2000);
+      // } else {
+      //   setTimeout(() => {
+      //     setBounceOutDown(false);
+      //   }, 12000);
+      // }
+    })
+  );
+}
 
   useEffect(() => {
     fetchData();
   }, []);
+
+
+
 
   return (
     <GlobalContext.Provider
